3779ee12068acb972fafab06e176b10a20583965
zige <zige@pinduoduo.com>
2021-03-16 21:34:07 +0800

    实现一个自定义 JMX MBean，通过 Jolokia 做 Servlet 代理

--- projects/stage-0/user-platform/user-web/src/main/java/org/geektimes/projects/user/web/listener/ComponentContextInitializerListener.java
+++ projects/stage-0/user-platform/user-web/src/main/java/org/geektimes/projects/user/web/listener/ComponentContextInitializerListener.java
@@ -1,10 +1,19 @@
 package org.geektimes.projects.user.web.listener;
 
 import org.geektimes.context.ComponentContext;
+import org.geektimes.projects.user.domain.User;
+import org.geektimes.projects.user.management.UserManager;
 
+import javax.management.InstanceAlreadyExistsException;
+import javax.management.MBeanRegistrationException;
+import javax.management.MBeanServer;
+import javax.management.MalformedObjectNameException;
+import javax.management.NotCompliantMBeanException;
+import javax.management.ObjectName;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
+import java.lang.management.ManagementFactory;
 
 /**
  * {@link ComponentContext} 初始化器
@@ -19,6 +28,14 @@ public class ComponentContextInitializerListener implements ServletContextListen
         this.servletContext = sce.getServletContext();
         ComponentContext context = new ComponentContext();
         context.init(servletContext);
+
+        MBeanServer mBeanServer = ManagementFactory.getPlatformMBeanServer();
+        User user = new User();
+        try {
+            mBeanServer.registerMBean(new UserManager(user), new ObjectName("org.geektimes.projects.user.management:type=User"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
